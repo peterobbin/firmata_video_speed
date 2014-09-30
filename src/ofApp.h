a2c452efb9914741db31cda49941a2a412e0208f
peterobbin <beijingwang@gmail.com>
2014-09-30 19:52:42 -0400

    arduino_of_setup_ready

---
+++ src/ofApp.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "ofMain.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+    
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+    
+    ofArduino ard;
+    ofTrueTypeFont		font;
+    ofTrueTypeFont      smallFont;
+    bool      bSetupArduino;
+    
+    
+    ofVideoPlayer fingerMovie;
+    bool frameByframe;
+    
+    
+    
+private:
+    
+    void setupArduino(const int & version);
+    void digitalPinChanged(const int & pinNum);
+    void analogPinChanged(const int & pinNum);
+    void updateArduino();
+    
+    string buttonState;
+    string potValue;
+		
+};
